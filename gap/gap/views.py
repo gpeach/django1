a0f0088077f82e6fa76a81adfab44a3c4a764332
ghardinpeach <gary.a.peach@gmail.com>
2024-05-24 11:04:17 -0400

    initial commit

---
+++ gap/gap/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    topic_value = request.GET.get('topic', '')
+    return HttpResponse(f"<body>welcome to my {topic_value} home page.</body>")
