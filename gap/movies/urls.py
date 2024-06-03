6f95542e5d9103460a5a0946b267a30437d92b6a
ghardinpeach <gary.a.peach@gmail.com>
2024-06-03 11:45:03 -0400

    sharing templates, detail page

--- gap/movies/urls.py
+++ gap/movies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from . import views
 
+app_name = 'movies'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail'),
 ]
